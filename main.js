dbcdfcd26a8f349ad936e3e5491a154d1b733275
Michael <msiller455@gmail.com>
2021-08-20 12:36:01 -0700

    adds main, shuffle, render functions and starting array

--- main.js
+++ main.js
@@ -1,1 +1,40 @@
-console.log('js hooked up')
+
+const startArray = [...Array(10).keys()]
+const currentArray = shuffle(startArray)
+const currentArrayDiv = document.getElementById('currentArray')
+
+function main() {
+    render()
+}
+
+function shuffle(arr) {
+    // Fisher Yates Shuffle
+    let count = arr.length, idx, currentElem
+
+    while(count) {
+        idx = Math.floor(Math.random() * count--)
+        currentElem = arr[count]
+        arr[count] = arr[idx]
+        arr[idx] = currentElem
+    }
+
+    return arr
+}
+
+function render() {
+    let arrString
+    currentArray.forEach((num, idx) => {
+        if(!idx) {
+            arrString = '[ ' + num + ', '
+        } else if(idx === currentArray.length - 1) {
+            arrString += num + ' ]'
+        } else {
+            arrString += num + ', '
+        }
+    })
+
+    currentArrayDiv.innerText = "The current array is " + arrString
+}
+
+
+main()
