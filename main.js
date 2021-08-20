48dc22ffe77a1f29dd59aa62058c6046c8752bb5
Michael <msiller455@gmail.com>
2021-08-20 14:51:50 -0700

    adds ui, and bubble sort functionality

--- main.js
+++ main.js
@@ -1,14 +1,70 @@
 
-const startArray = [...Array(10).keys()]
-const currentArray = shuffle(startArray)
-const currentArrayDiv = document.getElementById('currentArray')
+// State
+const startArray = [...Array(15).keys()]
+let currentArray = shuffle(startArray)
+let bubbleSortArray, bubbleSortSwaps
+
+// DOM Elements
+const currentArrayEl = document.getElementById('currentArray')
+const bubbleSortEl = document.getElementById('bubbleSort')
+const bubbleSortBtn = document.getElementById('bubbleSortBtn')
+const randomizeBtn = document.getElementById('randomizeArray')
 
 function main() {
     render()
+
+    bubbleSortBtn.addEventListener('click', () => {
+        [ bubbleSortArray, bubbleSortSwaps ] = bubbleSort(currentArray)
+        render()
+    })
+
+    randomizeBtn.addEventListener('click', () => {
+        currentArray = shuffle(currentArray)
+        bubbleSortArray = bubbleSortSwaps = null
+        render()
+    })
+}
+
+function render() {
+    currentArrayEl.innerText = "The current array is " + arrToString(currentArray)
+
+    bubbleSortEl.children[2].innerText = ''
+    bubbleSortEl.children[3].innerText = ''
+    if(bubbleSortArray) {
+        bubbleSortEl.children[2].innerText = 'The sorted array is ' + arrToString(bubbleSortArray)
+        bubbleSortEl.children[3].innerText = 'The number of times swapped was ' + bubbleSortSwaps        
+    }
+}
+
+// Algos
+
+function bubbleSort(array) {
+    const arr = [...array]
+    let numSwaps = 0
+    const length = arr.length
+
+    for(let i = 0; i < length - 1; i++) {
+        for(let j = 0; j < length - 1 - i; j++) {
+            if(arr[j] > arr[j + 1]) {
+                numSwaps++
+                swap(arr, j, j+1)
+            }
+        }
+    }
+
+    return [arr, numSwaps]
+}
+
+// Helper functions
+
+function swap(arr, currIdx, swapIdx) {
+    const temp = arr[currIdx]
+    arr[currIdx] = arr[swapIdx]
+    arr[swapIdx] = temp
 }
 
 function shuffle(arr) {
-    // Fisher Yates Shuffle
+    // Fisher Yates
     let count = arr.length, idx, currentElem
 
     while(count) {
@@ -21,9 +77,10 @@ function shuffle(arr) {
     return arr
 }
 
-function render() {
+function arrToString(arr) {
     let arrString
-    currentArray.forEach((num, idx) => {
+    
+    arr.forEach((num, idx) => {
         if(!idx) {
             arrString = '[ ' + num + ', '
         } else if(idx === currentArray.length - 1) {
@@ -33,7 +90,7 @@ function render() {
         }
     })
 
-    currentArrayDiv.innerText = "The current array is " + arrString
+    return arrString
 }
 
 
